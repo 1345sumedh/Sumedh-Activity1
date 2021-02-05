13da2ee48dda16c0cbfb8049e503650cf48dbbcc
Sumedh Hagaldivte <65391322+1345sumedh@users.noreply.github.com>
2021-02-05 16:21:22 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,1 +1,31 @@
 //WAP to find the distance between two point using 4 functions.
+#include<stdio.h>
+#include<math.h>
+
+int ask_value()
+{
+float a;
+printf("Enter the coordinates:\n");
+scanf("%f",&a);
+return (a);
+}
+
+float distance(float x1,float y1,float x2,float y2)
+{
+float d;
+d= sqrt((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
+return(d);
+}
+
+void show(float d)
+{
+printf("The distance between points is %f",d);
+}
+int main()
+{
+float x1,y1,x2,y2,d;
+x1= ask_value();y1= ask_value();x2= ask_value();y2= ask_value();
+d= distance(x1,y1,x2,y2);
+show(d);
+return 0;
+}
