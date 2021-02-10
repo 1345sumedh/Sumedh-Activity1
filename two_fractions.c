105d485fcbcd34b7c10324af4a72a295b6981ba9
Sumedh Hagaldivte <65391322+1345sumedh@users.noreply.github.com>
2021-02-10 22:56:21 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -2,37 +2,59 @@
 #include <stdio.h>
 #include<stdlib.h>
 
+#include <stdio.h>
+#include<stdlib.h>
+
 struct frac
 {
-int n;
-int d;
+    int n;
+    int d;
 };
 
 int ask_value()
 {
-float a;
-printf("Enter the value:\n");
-scanf("%f",&a);
-return (a);
+    float a;
+    printf("Enter the value:\n");
+    scanf("%f",&a);
+    return (a);
 }
 
 struct frac cal(struct frac f1 ,struct frac f2)
 {
-struct frac addi ={((f1.n*f2.d)+(f2.n*f1.d)),(f1.d*f2.d)};
-return addi;
+    struct frac addi ={((f1.n*f2.d)+(f2.n*f1.d)),(f1.d*f2.d)};
+    return addi;
 }
 
 struct frac show(struct frac sum)
 {
-printf("The sum of fractions is : %d/%d",sum.n,sum.d);
+    int max,max1;
+    if(sum.n==sum.d)
+    max1 = sum.n;
+    else if(sum.n <sum.d)
+    max = sum.d;
+    else
+    max= sum.n;
+    for(int i= max;i>1;i--)
+    {
+        if(sum.n%i==0 && sum.d%i== 0)
+        {
+            max1 = i;
+            break;
+        }
+    }
+    
+    sum.n = sum.n / max1;
+    sum.d = sum.d / max1;
+    
+    printf("The sum of fractions is : %d/%d",sum.n,sum.d);
 }
 
 int main()
 {
-
-struct frac f1 ,f2;
-f1.n=ask_value();f1.d=ask_value();f2.n=ask_value();f2.d=ask_value();
-struct frac sum = cal(f1,f2);
-show(sum);
-return 0;
+    
+    struct frac f1 ,f2;
+    f1.n=ask_value();f1.d=ask_value();f2.n=ask_value();f2.d=ask_value();
+    struct frac sum = cal(f1,f2);
+    show(sum);
+    return 0;
 }
