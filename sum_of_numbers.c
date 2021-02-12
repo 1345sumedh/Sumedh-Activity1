bc629f40a5cd4dedade6593af6e9d79ef20afcd0
Sumedh Hagaldivte <65391322+1345sumedh@users.noreply.github.com>
2021-02-12 07:41:28 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,44 +1,43 @@
-//Write a program to find the sum of n different numbers using 4 functions
-#include <stdio.h>
-
+#include<stdio.h>
 int ask_n()
 {
-int n1;
-printf("enter the number of elements: ");
-scanf("%d",&n1);
-return n1;
+    int n1;
+    printf("enter the number of elements: ");
+    scanf("%d",&n1);
+    return n1;
 }
 
-void ask_value(int n, int a[])
+void ask_value(int n, float a[])
 {
-printf("enter %d elements:\n",n);
-for(int i=0;i<n;i++)
-{
-scanf("%d",&a[i]);
-}
-}
-
-int cal(int a[],int n)
-{
-int sum1=0;
-for(int i =0;i<n;i++)
-{
-sum1=sum1+a[i];   
-}
-return sum1;
+    printf("enter %d elements:\n",n);
+    for(int i =0;i<n;i++)
+    {
+        scanf("%f",&a[i]);
+    }
 }
 
-void show(int s)
+float cal(float a[],int n)
 {
-printf("Sum of n terms is: %d",s);
+    float sum1=0;
+    for(int i=0;i<n;i++)
+    {
+        sum1=sum1+a[i];
+    }
+    return sum1;
 }
+
+void show(float s)
+{
+    printf("Sum of n terms is: %f",s);
+}
+
 
 int main()
 {
-int n = ask_n(); int a[n];
-ask_value(n,a);
-int sum = cal(a,n);
-show(sum);
-
-return 0;
+    int n = ask_n();
+    float a[n];
+    ask_value(n,a);
+    float sum = cal(a,n);
+    show(sum);
+    return 0;
 }
