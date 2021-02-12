a2f79cbf991d925801a825e6e51bb847517abb6c
Sumedh Hagaldivte <65391322+1345sumedh@users.noreply.github.com>
2021-02-12 06:48:19 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -4,35 +4,37 @@
 #include<math.h>
 struct point
 {
-float x;
-float y;
+    float x;
+    float y;
 };
 
-int ask_value()
+float ask_value()
 {
-float a;
-printf("Enter the coordinate:\n");
-scanf("%f",&a);
-return (a);
+    float a;
+    printf("Enter the coordinate:\n");
+    scanf("%f",&a);
+    return (a);
 }
 
 float distance(struct point p1,struct point p2)
 {
-float d;
-d = sqrt(((p2.x-p1.x)*(p2.x-p1.x))+((p2.y-p1.y)*(p2.y-p1.y)));
-return (d);
+    float d;
+    d = sqrt(((p2.x-p1.x)*(p2.x-p1.x))+((p2.y-p1.y)*(p2.y-p1.y)));
+    return (d);
 }
 
 void show(float d)
 {
-printf("The distance between points is: %f",d);
+    printf("The distance between points is: %f",d);
 }
+
 
 int main()
 {
-struct point p1,p2; 
-p1.x = ask_value();p1.y = ask_value();p2.x = ask_value();p2.y = ask_value();
-float d = distance(p1,p2);
-show(d);
-return 0;
+    struct point p1,p2; 
+    p1.x = ask_value();p1.y = ask_value();p2.x = ask_value();p2.y = ask_value();
+    float d = distance(p1,p2);
+    show(d);
+    return 0;
 }
+
