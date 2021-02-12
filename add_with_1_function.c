0ae51d78f80c012737f310ed28636dc52e10d5eb
Sumedh Hagaldivte <65391322+1345sumedh@users.noreply.github.com>
2021-02-12 07:48:05 +0530

    Update add_with_1_function.c

--- add_with_1_function.c
+++ add_with_1_function.c
@@ -2,10 +2,10 @@
 #include<stdio.h>
 int main()
 {
-  int a,b,sum=0;
+  float a,b;
   printf("Enter two numbers:\n");
-  scanf("%d %d",&a,&b);
-  sum=a+b;
-  printf("Adittion of %d and %d is %d",a,b,sum);
+  scanf("%f %f",&a,&b);
+  float sum=a+b;
+  printf("Addition of %f and %f is %f",a,b,sum);
   return 0;
 }
